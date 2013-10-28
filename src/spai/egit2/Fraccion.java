ec59ff9f15a9643afead3a1112b1ebc5aff1aa8d
PabloLerma <plermam@gmail.com>
2013-10-28 01:22:55 +0100

    Corregido un problema con el encoding.

--- src/spai/egit2/Fraccion.java
+++ src/spai/egit2/Fraccion.java
@@ -11,22 +11,22 @@ package spai.egit2;
  * producto de medios
  * 
  * Las fracciones irreducibles son aquellas que no se pueden simplificar, esto
- * sucede cuando el numerador y el denominador son primos entre s�
+ * sucede cuando el numerador y el denominador son primos entre sí
  * 
- * Reducir varias fracciones a com�n denominador consiste en convertirlas en
+ * Reducir varias fracciones a común denominador consiste en convertirlas en
  * otras equivalentes que tengan el mismo denominador
  * 
  * Comparar fracciones
  * 
- * Suma fracciones: En primer lugar se reducen los denominadores a com�n
+ * Suma fracciones: En primer lugar se reducen los denominadores a común
  * denominador, y se suman o se restan los numeradores de las fracciones
  * equivalentes obtenidas
  * 
- * Multiplicaci�n: La multiplicaci�n de dos fracciones es otra fracci�n que
+ * Multiplicación: La multiplicación de dos fracciones es otra fracción que
  * tiene: Por numerador el producto de los numeradores. Por denominador el
  * producto de los denominadores.
  * 
- * La divisi�n de dos fracciones es otra fracci�n que tiene: Por numerador el
+ * La división de dos fracciones es otra fracción que tiene: Por numerador el
  * producto de los extremos. Por denominador el producto de los medios. Invertir
  * fraccion
  * 
@@ -34,38 +34,38 @@ package spai.egit2;
  * 
  */
 public class Fraccion {
-    private int numerador;
+	private int numerador;
 
-    private int denominador;
+	private int denominador;
 
-    public Fraccion(int numerador, int denominador) {
-        this.numerador = numerador;
-        this.denominador = denominador;
-    }
+	public Fraccion(int numerador, int denominador) {
+		this.numerador = numerador;
+		this.denominador = denominador;
+	}
 
-    public Fraccion() {
-        this(1, 1);
-    }
+	public Fraccion() {
+		this(1, 1);
+	}
 
-    public int getNumerador() {
-        return numerador;
-    }
+	public int getNumerador() {
+		return numerador;
+	}
 
-    public void setNumerador(int numerador) {
-        this.numerador = numerador;
-    }
+	public void setNumerador(int numerador) {
+		this.numerador = numerador;
+	}
 
-    public int getDenominador() {
-        return denominador;
-    }
+	public int getDenominador() {
+		return denominador;
+	}
 
-    public void setDenominador(int denominador) {
-        this.denominador = denominador;
-    }
+	public void setDenominador(int denominador) {
+		this.denominador = denominador;
+	}
 
-    public double decimal() {
-        return (double) numerador / denominador;
-    }
+	public double decimal() {
+		return (double) numerador / denominador;
+	}
 
 	public boolean isPropia() {
 		// TODO Auto-generated method stub
@@ -74,12 +74,12 @@ public class Fraccion {
 
 	public void multiplicar(Fraccion fraccionMult) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void dividir(Fraccion fraccionDiv) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
